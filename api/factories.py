7fb781ccca45b44231c5192fde9db5570c836c4d
sweldon <stmydchannel@gmail.com>
2019-12-17 21:15:44 -0500

    Added factories for tests

---
+++ api/factories.py
@@ -0,0 +1,5 @@
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    pass
---
+++ api/tests.py
@@ -0,0 +1,6 @@
+from rest_framework.test import APITestCase
+
+
+class UserLoginTest(APITestCase):
+    def setUp(self):
+        pass
