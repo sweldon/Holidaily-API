1821c4dd17e33ce1dbdee82fe7afd10210c370a3
sweldon <stmydchannel@gmail.com>
2020-02-11 10:09:37 -0500

    Updated daily push to filter on active holidays only

--- api/management/commands/daily_push.py
+++ api/management/commands/daily_push.py
@@ -9,16 +9,25 @@ from holidaily.settings import APPCENTER_API_KEY
 class Command(BaseCommand):
     def handle(self, *args, **options):
         today = timezone.now().date()
-        todays_holidays = Holiday.objects.filter(date=today)
+        todays_holidays = Holiday.objects.filter(date=today, active=True)
         random_day = random.choice(todays_holidays)
         push = random_day.push if random_day.push else "Check out today's holidays!"
         day_name = random_day.name
         id = random_day.id
-        android_url = 'https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-Android-Dev/push/notifications'
-        ios_url = 'https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-IOS/push/notifications'
-        data = {"notification_content": {"name": "Daily Holiday Update", "title": day_name, "body": push,
-                                         "custom_data": {"holiday_id": id, "holiday_name": day_name}},
-                "notification_target": None}
+        android_url = (
+            "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-Android-Dev/push/"
+            "notifications"
+        )
+        ios_url = "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-IOS/push/notifications"
+        data = {
+            "notification_content": {
+                "name": "Daily Holiday Update",
+                "title": day_name,
+                "body": push,
+                "custom_data": {"holiday_id": id, "holiday_name": day_name},
+            },
+            "notification_target": None,
+        }
         headers = {"X-API-Token": APPCENTER_API_KEY}
-        r_android = requests.post(android_url, headers=headers, json=data)
-        r_ios = requests.post(ios_url, headers=headers, json=data)
+        requests.post(android_url, headers=headers, json=data)
+        requests.post(ios_url, headers=headers, json=data)
