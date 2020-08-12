23c328b25b5efa53ebba37335e1bd0650065f1a0
sweldon <stmydchannel@gmail.com>
2020-08-12 09:04:02 -0400

    Removed appcenter

--- holidaily/settings.py
+++ holidaily/settings.py
@@ -144,18 +144,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "holidailyapp@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ["email_pass"]
 EMAIL_PORT = 587
-STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../static")
 STATICFILES_DIRS = (("base", os.path.join(STATIC_ROOT, "base").replace("\\", "/")),)
-
-# Push notifications
-APPCENTER_API_KEY = os.environ["appcenter_api"]
-PUSH_ENDPOINT_ANDROID = (
-    "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-Android-Dev/"
-    "push/notifications/"
-)
-
-PUSH_ENDPOINT_IOS = "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-IOS/push/notifications"
 
 SLACK_CLIENT = slack.WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
