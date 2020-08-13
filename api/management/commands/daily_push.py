31000666bab7659c2da5e88f2589316c26a1e07b
sweldon <stmydchannel@gmail.com>
2020-08-13 11:26:20 -0400

    Removed appcenter logic from daily push

--- api/management/commands/daily_push.py
+++ api/management/commands/daily_push.py
@@ -1,12 +1,9 @@
 from django.core.management.base import BaseCommand
 from push_notifications.models import GCMDevice, APNSDevice
 
-from api.models import Holiday, UserProfile
+from api.models import Holiday
 from django.utils import timezone
-import requests
 import random
-from holidaily.settings import APPCENTER_API_KEY
-from django.db.models.functions import Length
 
 
 class Command(BaseCommand):
@@ -24,34 +21,9 @@ class Command(BaseCommand):
         day_name = random_day.name
         id = random_day.id
 
-        # Old AppCenter Logic
-        old_devices = list(
-            UserProfile.objects.annotate(text_len=Length("device_id"))
-            .filter(text_len=36)
-            .values_list("device_id", flat=True)
-        )
-        android_url = (
-            "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-Android-Dev/push/"
-            "notifications"
-        )
-        ios_url = "https://api.appcenter.ms/v0.1/apps/steven.d.weldon-gmail.com/Holidaily-IOS/push/notifications"
-        data = {
-            "notification_content": {
-                "name": "Daily Holiday Update",
-                "title": day_name,
-                "body": push,
-                "custom_data": {"holiday_id": id, "holiday_name": day_name},
-            },
-            "notification_target": {"type": "devices_target", "devices": old_devices},
-        }
-        headers = {"X-API-Token": APPCENTER_API_KEY}
-        requests.post(android_url, headers=headers, json=data)
-        requests.post(ios_url, headers=headers, json=data)
-
-        # New FCM/APNs logic
-        # Now send to new devices, only if the user has gotten a new token
-        androids = GCMDevice.objects.all().exclude(registration_id__in=old_devices)
-        iphones = APNSDevice.objects.all().exclude(registration_id__in=old_devices)
+        # FCM/APNs logic
+        androids = GCMDevice.objects.all()
+        iphones = APNSDevice.objects.all()
 
         iphones.send_message(
             message={"title": day_name, "body": push},
