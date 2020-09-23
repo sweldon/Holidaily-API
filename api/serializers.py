53bb387d32f7930338d542392f537f8c756feea8
sweldon <stmydchannel@gmail.com>
2020-09-23 17:44:59 -0400

    Relative timestamp on holiday for legacy

--- api/serializers.py
+++ api/serializers.py
@@ -265,14 +265,14 @@ class HolidaySerializer(serializers.ModelSerializer):
     def get_image(self, obj):
         return f"{CLOUDFRONT_DOMAIN}/{obj.image_name}"
 
-    # def get_time_since(self, obj):
-    #     time_ago = humanize.naturaltime(timezone.now().date() - obj.date)
-    #     return normalize_time(time_ago, "relative")
-
     def get_time_since(self, obj):
-        if obj.date == timezone.now().date():
-            return "Today"
-        return obj.date.strftime("%b %d, %Y")
+        time_ago = humanize.naturaltime(timezone.now().date() - obj.date)
+        return normalize_time(time_ago, "relative")
+
+    # def get_time_since(self, obj):
+    #     if obj.date == timezone.now().date():
+    #         return "Today"
+    #     return obj.date.strftime("%b %d, %Y")
 
     def get_celebrating(self, obj):
         username = self.context.get("username", None)
